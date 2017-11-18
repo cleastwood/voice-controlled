103a7e6d5d6fffe2dab805647cecdda575e833c8
cleastwood <narsilus@gmail.com>
2017-11-18 10:21:03 +0100

    Before applying speech

---
+++ Dyplomowa v2/Assets/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class PlayerMovement : MonoBehaviour{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*public class PlayerNavigation : MonoBehaviour
+{
+
+    public Camera cam;
+    public NavMeshAgent navMeshAgent;
+    // Use this for initialization
+    void Start()
+    {
+        cam = Camera.main;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
+*/
